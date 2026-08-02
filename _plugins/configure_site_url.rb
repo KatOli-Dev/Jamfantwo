ed3393bf67f5da2453143c3fcb706a353c17dac2
kat_dash_oli <kat_dash_oli@proton.me>
2026-08-02 00:09:35 -0400

    Use local network URL during development

--- _plugins/configure_site_url.rb
+++ _plugins/configure_site_url.rb
@@ -1,8 +1,26 @@
+require 'socket'
+
+def local_site_url
+  addresses = Socket.getifaddrs.map(&:addr).compact.select do |address|
+    address.ipv4? && !address.ipv4_loopback?
+  end.map(&:ip_address)
+  private_address = addresses.find { |address| address.start_with?('192.168.') }
+  private_address ||= addresses.find do |address|
+    address.start_with?('10.') || address.match?(/\A172\.(1[6-9]|2\d|3[01])\./)
+  end
+  private_address ||= '127.0.0.1'
+  "http://#{private_address}:#{ENV.fetch('JEKYLL_PORT', '4000')}"
+end
+
 Jekyll::Hooks.register :site, :after_init do |site|
   production_url = ENV['JEKYLL_SITE_URL']
-  next if production_url.nil? || production_url.empty?
-
-  production_url = production_url.chomp('/')
-  site.config['url'] = production_url
-  site.config['site_url'] = production_url
+  if production_url && !production_url.empty?
+    production_url = production_url.chomp('/')
+    site.config['url'] = production_url
+    site.config['site_url'] = production_url
+  elsif Jekyll.env != 'production' && site.config['site_url'] == site.config['url']
+    local_url = local_site_url
+    site.config['url'] = local_url
+    site.config['site_url'] = local_url
+  end
 end
