c1187c3995bca7021f332f98810c1f69f32e4c6a
kat_dash_oli <kat_dash_oli@proton.me>
2026-08-02 12:55:07 -0400

    Fix mobile categories dropdown

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -11,7 +11,7 @@ var dt = document.querySelector('.dropdown-toggle');
 var dm = document.querySelector('.dropdown-menu');
 if (dt && dm) {
   dt.addEventListener('click', function(e) {
-    if (window.innerWidth <= 600) {
+    if (window.innerWidth <= 850) {
       var expanded = dt.getAttribute('aria-expanded') === 'true';
       dt.setAttribute('aria-expanded', !expanded);
       dm.classList.toggle('dropdown-menu-open');
